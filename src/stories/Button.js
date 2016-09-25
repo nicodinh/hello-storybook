7206cae6e1e43bf51294c8f7e768c91757eb5389
AZARI Nicolas <nicodinh@azari.fr>
2016-09-25 10:10:29 +0200

    minor changes

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 const buttonStyles = {
   border: '1px solid #eee',
   borderRadius: 3,
-  backgroundColor: '#FFFFFF',
+  backgroundColor: '#da3939',
   cursor: 'pointer',
   fontSize: 15,
   padding: '3px 10px',
